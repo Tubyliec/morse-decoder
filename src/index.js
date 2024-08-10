b12820cf0e6ab16fcb915f3333f448a106af7a02
tubyliec <antikruk@vivaldi.net>
2024-08-10 19:19:59 +0300

    feat: add task code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+let arr = [];
+let start = 0;
+for (let i = 0; i < expr.length / 10; i++) {
+    arr[i] = expr.slice(start, start + 10);
+    start += 10;
+}
+
+console.log(arr)
+
+function deleteZero (item) {
+    return item.replace(/[0]+/, '');
+}
+
+let newArr = arr.map(deleteZero);
+
+function replaceTen (item) {
+    return item.replace(/10/g, '.');
+}
+
+let arrWithDot = newArr.map(replaceTen);
+
+function replaceEleven (item) {
+    return item.replace(/11/g, '-');
+}
+
+let arrWithDash = arrWithDot.map(replaceEleven);
+
+function replaceAsterix (item) {
+    return item.replace("**********", ' ');
+}
+
+let arrWithSpace = arrWithDash.map(replaceAsterix);
+
+let endString = arrWithSpace.join(' ');
+
+return endString;
+
 }
 
 module.exports = {
